627602949ed32bc35f30435ab0e65aa379844ffd
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-06-11 01:46:08 +0600

    adminRoute added

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,14 +1,14 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaBook, FaUsers } from 'react-icons/fa';
 import useCart from "../hooks/useCart";
-// import useAdmin from "../hooks/useAdmin";
+import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
     const [cart] = useCart();
 
     // TODO: load data from the server to have dynamic isAdmin based on Data
-    const isAdmin = true;
-    // const [isAdmin] = useAdmin();
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
